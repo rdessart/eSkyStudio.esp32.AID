4683dcc13d9b8b73fcf355d06f91a37fec4faf4f
Romain DESSART <romaindessart@gmail.com>
2025-01-08 17:56:13 +0100

    removed log messages

--- main/main.c
+++ main/main.c
@@ -17,64 +17,57 @@
 
 int client_sock = -1;
 
-#define TCP_PORT            8766
-#define UART_NUM            UART_NUM_0
-#define UART_TX_PIN         1
-#define UART_RX_PIN         3
-#define UART_BUFFER_SIZE    1024
-#define EXAMPLE_ESP_WIFI_SSID      "eSkyAID"
-#define EXAMPLE_ESP_WIFI_PASS      "eSkyAID1234"
-#define EXAMPLE_ESP_WIFI_CHANNEL   2
-#define EXAMPLE_MAX_STA_CONN       4
+#define TCP_PORT                   8766
+#define UART_NUM                   UART_NUM_0
+#define UART_TX_PIN                1
+#define UART_RX_PIN                3
+#define UART_BUFFER_SIZE           1024
+#define WIFI_SSID      "eSky_aircraft"
+#define WIFI_PASS      "eSky1234"
+#define WIFI_CHANNEL   2
+#define MAX_STA_CONN       4
 
-static const char *TAG = "wifi softAP";
 
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
 {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
+        printf("#AP;CONNECTION;%s;%d\n",event->mac, event->aid);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d, reason=%d",
-                 MAC2STR(event->mac), event->aid, event->reason);
+        printf("#AP;LEAVE;%s;%d\n",event->mac, event->aid);
     }
 }
 
 void wifi_init_softap(void)
 {
     
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_init();
+    esp_event_loop_create_default();
 
     esp_netif_t *netif = esp_netif_create_default_wifi_ap();
 
     esp_netif_ip_info_t ip_info;
-    IP4_ADDR(&ip_info.ip, 192, 168, 5, 1);      // Set IP address
-    IP4_ADDR(&ip_info.gw, 192, 168, 5, 1);      // Set gateway
+    IP4_ADDR(&ip_info.ip, 172, 20, 192, 1);      // Set IP address
+    IP4_ADDR(&ip_info.gw, 172, 20, 192, 1);      // Set gateway
     IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0); // Set netmask
-    ESP_ERROR_CHECK(esp_netif_dhcps_stop(netif)); // Stop DHCP server
-    ESP_ERROR_CHECK(esp_netif_set_ip_info(netif, &ip_info)); // Set static IP
-    ESP_ERROR_CHECK(esp_netif_dhcps_start(netif)); // Restart DHCP server
+    esp_netif_dhcps_stop(netif); // Stop DHCP server
+    esp_netif_set_ip_info(netif, &ip_info); // Set static IP
+    esp_netif_dhcps_start(netif); // Restart DHCP server
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_wifi_init(&cfg);
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &wifi_event_handler,
-                                                        NULL,
-                                                        NULL));
+    esp_event_handler_instance_register(WIFI_EVENT,ESP_EVENT_ANY_ID,&wifi_event_handler,NULL,NULL);
 
     wifi_config_t wifi_config = {
         .ap = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
-            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            .max_connection = EXAMPLE_MAX_STA_CONN,
+            .ssid = WIFI_SSID,
+            .ssid_len = strlen(WIFI_SSID),
+            .channel = WIFI_CHANNEL,
+            .password = WIFI_PASS,
+            .max_connection = MAX_STA_CONN,
             .authmode = WIFI_AUTH_WPA3_PSK,
             .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
             .pmf_cfg = {
@@ -83,16 +76,14 @@ void wifi_init_softap(void)
         },
     };
 
-    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+    if (strlen(WIFI_PASS) == 0) {
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+    esp_wifi_set_mode(WIFI_MODE_AP);
+    esp_wifi_set_config(WIFI_IF_AP, &wifi_config);
+    esp_wifi_start();
+    printf("#OK;SSID:%s;password:%s;Channel:%d\n", WIFI_SSID, WIFI_PASS, WIFI_CHANNEL);
 }
 
 void tcp_server_task(void *pvParameters)
@@ -104,7 +95,7 @@ void tcp_server_task(void *pvParameters)
     // Create socket
     int server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
     if (server_sock < 0) {
-        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        printf("#ERROR;Unable to create socket;%d\n", errno);
         vTaskDelete(NULL);
         return;
     }
@@ -113,45 +104,39 @@ void tcp_server_task(void *pvParameters)
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(TCP_PORT);
 
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
+    {
+        printf("#ERROR;bind;%d\n", errno);
         close(server_sock);
         vTaskDelete(NULL);
         return;
     }
 
-    if (listen(server_sock, 1) < 0) {
-        ESP_LOGE(TAG, "Error during listen: errno %d", errno);
+    if (listen(server_sock, 1) < 0) 
+    {
+        printf("#ERROR;listen;%d\n", errno);
         close(server_sock);
         vTaskDelete(NULL);
         return;
     }
 
-    ESP_LOGI(TAG, "TCP server listening on port %d", TCP_PORT);
+    printf("#OK;READY!\n");
 
     while (1) 
     {
         client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_len);
-        if (client_sock < 0) {
-            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+        if (client_sock < 0) 
+        {
+            printf("#ERROR;accept;%d\n", errno);
             break;
         }
-
-        ESP_LOGI(TAG, "Client connected.");
-
+        printf("#CONNECTED;%d\n", client_sock);
         while (1) {
-            // Receive data (if any)
             int len = recv(client_sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-            if (len < 0) {
-                ESP_LOGE(TAG, "recv failed: errno %d", errno);
-                break;
-            } else if (len == 0) {
-                ESP_LOGI(TAG, "Client disconnected.");
-                break;
-            }
+            if(len <= 0) break;
 
             rx_buffer[len] = '\0';
-            ESP_LOGI(TAG, "Received: %s", rx_buffer);
+            uart_write_bytes(UART_NUM, rx_buffer, len);
         }
 
         close(client_sock);
@@ -171,7 +156,6 @@ void uart_to_tcp_task(void *pvParameters) {
         if (len > 0 && client_sock >= 0) 
         {
             send(client_sock, data, len, 0);
-            ESP_LOGI(TAG, "Forwarded %d bytes to client", len);
         }
     }
 }
@@ -180,12 +164,9 @@ void app_main(void)
 {
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
+      nvs_flash_erase();
       ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK(ret);
-
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
 
      uart_config_t uart_config = {
         .baud_rate = 115200,
